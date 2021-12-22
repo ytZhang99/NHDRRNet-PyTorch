4516922794d0b1e68b7aff7366aafc141d686092
ytZhang99 <yutongzhang@buaa.edu.cn>
2021-12-22 15:07:07 +0800

    update

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import sys
 import traceback
@@ -36,42 +35,3 @@ class Logger(object):
     def close(self):
         if self.file is not None:
             self.file.close()
-=======
-import os
-import sys
-import traceback
-
-
-class Logger(object):
-    '''Save training process to log file with simple plot function.'''
-
-    def __init__(self, fpath, resume=False):
-        self.file = None
-        self.resume = resume
-        if os.path.isfile(fpath):
-            if resume:
-                self.file = open(fpath, 'a')
-            else:
-                self.file = open(fpath, 'w')
-        else:
-            self.file = open(fpath, 'w')
-
-    def append(self, target_str):
-        if not isinstance(target_str, str):
-            try:
-                target_str = str(target_str)
-            except:
-                traceback.print_exc()
-            else:
-                # print(target_str)
-                self.file.write(target_str + '\n')
-                self.file.flush()
-        else:
-            # print(target_str)
-            self.file.write(target_str + '\n')
-            self.file.flush()
-
-    def close(self):
-        if self.file is not None:
-            self.file.close()
->>>>>>> 320cf2305e588f0eba31d550eec35c4282346f15
